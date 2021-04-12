04ba8ece312eb0a9f8de0e23196fac0d5a16a546
houdini91 <mdstrauss91@gmail.com>
2021-04-12 15:29:37 +0300

    something to test with

---
+++ core.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+class JenkinsGraph:
+    BASE_DIR = "tmp/graph"
+    EXT = ".csv"
+    def __init__(self, series, name):
+        Path(self.BASE_DIR).mkdir(parents=True, exist_ok=True)
+        graph_path = os.path.join(self.BASE_DIR, name + self.EXT)
+        self.graph_path = graph_path
+        self.series = series
+
+    def local_plot(self, title=None):
+        self.series.plot(title=title)
+
+    def export_plot(self):
+        self.series.to_csv(self.graph_path, index = False)
+
+
+        
---
+++ simple.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from core import JenkinsGraph
+import pandas_datareader.data as web
+import datetime
+
+#if you get an error after executing the code, try adding below:
+pd.core.common.is_list_like = pd.api.types.is_list_like
+
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime.now()
+
+SP500 = web.DataReader(['sp500'], 'fred', start, end)
+SP500.dropna(inplace = True)
+SP500['daily_return'] = (SP500['sp500']/ SP500['sp500'].shift(1)) -1
+
+sp500_graph = JenkinsGraph(SP500['daily_return'], "sp500_daily_return")
+sp500_graph.local_plot("Daily_return")
+sp500_graph.export_plot()
+
+# SP500['daily_return'].to_csv(r'sp500_dail_return.csv', index = False)
+#Drop all Not a number values using drop method.
+# SP500['daily_return'].plot(title='S&P 500 daily returns')
+# plt.show()
