b23ce91639d218c7cf8f8864360d2ca6acb47460
houdini91 <mdstrauss91@gmail.com>
2021-04-13 17:40:01 +0300

    test

--- simple.py
+++ simple.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from core import JenkinsGraph
 import pandas_datareader.data as web
+from pandas_datareader.nasdaq_trader import get_nasdaq_symbols
 import datetime
 
 
@@ -18,47 +19,34 @@ class SingleStock:
     def dropna(self):
         self.df.dropna(inplace = True)
 
-    def dataReader(self, start_date, end_date=None, data_source=None):
-        start = start_date
-        if end_date == None:
-            end_date = datetime.datetime.now()
+    def dataReader(self, start, end=None, data_source=None):
+        if end == None:
+            end = datetime.datetime.now()
         if data_source == None:
             data_source = 'fred'
 
-        self.df = web.DataReader([self.name], data_source, start, end_date)
+        self.df = web.DataReader([self.name], data_source='fred', start=start, end=end)
         self.dropna()
 
     def dailyReturn(self):
         self.df['daily_return'] = (self.df[self.name]/ (self.df[self.name].shift(1)) -1)
         self.dropna()
 
+
+#St. Louis FED
+FED_tickers = ['sp500', 'VIXCLS']
+# Nasdaq Trader symbol
+NASDAQ_tickers = ['BA', 'IBM']
+
 if __name__ == "__main__":
-    stock = SingleStock('sp500')  
-    stock.dataReader(start_date=datetime.datetime(2010, 1, 1))
-    stock.dailyReturn()
-    stock.report()
 
-    stock2 = SingleStock('VIXCLS')  
-    stock2.dataReader(start_date=datetime.datetime(2010, 1, 1))
-    stock2.dailyReturn()
-    stock2.report()
+    for ticker in NASDAQ_tickers:
+        f = web.DataReader(ticker, 'stooq')
+        graph = JenkinsGraph(f.Close, ticker + "_close")
+        graph.export_html()
 
-    # plt.show()
-# #if you get an error after executing the code, try adding below:
-# pd.core.common.is_list_like = pd.api.types.is_list_like
-
-# start = datetime.datetime(2010, 1, 1)
-# end = datetime.datetime.now()
-
-# SP500 = web.DataReader(['sp500'], 'fred', start, end)
-# SP500.dropna(inplace = True)
-# SP500['daily_return'] = (SP500['sp500']/ SP500['sp500'].shift(1)) -1
-
-# sp500_graph = JenkinsGraph(SP500['daily_return'], "sp500_daily_return")
-# # sp500_graph.local_plot("Daily_return")
-# sp500_graph.export_plot()
-
-# SP500['daily_return'].to_csv(r'sp500_dail_return.csv', index = False)
-#Drop all Not a number values using drop method.
-# SP500['daily_return'].plot(title='S&P 500 daily returns')
-# plt.show()
+    for ticker in FED_tickers:
+        single = SingleStock(ticker)  
+        single.dataReader(start=datetime.datetime(2010, 1, 1))
+        single.dailyReturn()
+        single.report()
