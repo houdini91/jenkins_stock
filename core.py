8d9e08dae10407cda613257e7f8b7b89447a48c0
houdini91 <mdstrauss91@gmail.com>
2021-04-13 12:22:33 +0300

    Test

--- core.py
+++ core.py
@@ -1,20 +1,34 @@
 import os
 from pathlib import Path
+import pandas as pd
 
 class JenkinsGraph:
-    BASE_DIR = "tmp/graph"
-    EXT = ".csv"
+    CSV_DIR = "tmp/csv"
+    HTML_DIR = "tmp/html"
+    CSV_EXT = ".csv"
+    HTML_EXT = ".html"
     def __init__(self, series, name):
-        Path(self.BASE_DIR).mkdir(parents=True, exist_ok=True)
-        graph_path = os.path.join(self.BASE_DIR, name + self.EXT)
-        self.graph_path = graph_path
+        pd.options.plotting.backend = "plotly"
+        Path(self.CSV_DIR).mkdir(parents=True, exist_ok=True)
+        Path(self.HTML_DIR).mkdir(parents=True, exist_ok=True)
+        self.graph_path = os.path.join(self.CSV_DIR, name + self.CSV_EXT)
+        self.html_path = os.path.join(self.HTML_DIR, name + self.HTML_EXT)
         self.series = series
 
     def local_plot(self, title=None):
         self.series.plot(title=title)
 
-    def export_plot(self):
+    def export_csv(self):
         self.series.to_csv(self.graph_path, index = True)
 
+    def plot(self):
+        self.fig = self.series.plot()
+
+    def export_html(self):
+        self.plot()
+        self.fig.write_html(self.html_path,
+                full_html=False,
+                include_plotlyjs='cdn')
+        # self.fig.show()
 
         
--- simple.py
+++ simple.py
@@ -4,19 +4,50 @@ from core import JenkinsGraph
 import pandas_datareader.data as web
 import datetime
 
-#if you get an error after executing the code, try adding below:
-pd.core.common.is_list_like = pd.api.types.is_list_like
 
-start = datetime.datetime(2010, 1, 1)
-end = datetime.datetime.now()
+class SingleStock:
+    def __init__(self, name):
+        self.name = name
 
-SP500 = web.DataReader(['sp500'], 'fred', start, end)
-SP500.dropna(inplace = True)
-SP500['daily_return'] = (SP500['sp500']/ SP500['sp500'].shift(1)) -1
+    def report(self):
+        self.df_graph = JenkinsGraph(self.df['daily_return'], self.name + "_daily_return")
+        self.df_graph.export_csv()
+        # self.df_graph.local_plot("Daily_return")
+        self.df_graph.export_html()
 
-sp500_graph = JenkinsGraph(SP500['daily_return'], "sp500_daily_return")
-# sp500_graph.local_plot("Daily_return")
-sp500_graph.export_plot()
+    def dropna(self):
+        self.df.dropna(inplace = True)
+
+    def dataReader(self, start_date):
+        start = start_date
+        end = datetime.datetime.now()
+        self.df = web.DataReader([self.name], 'fred', start, end)
+        self.dropna()
+
+    def dailyReturn(self):
+        self.df['daily_return'] = (self.df[self.name]/ (self.df[self.name].shift(1)) -1)
+        self.dropna()
+
+if __name__ == "__main__":
+    stock = SingleStock('sp500')  
+    stock.dataReader(start_date=datetime.datetime(2010, 1, 1))
+    stock.dailyReturn()
+    stock.report()
+
+    # plt.show()
+# #if you get an error after executing the code, try adding below:
+# pd.core.common.is_list_like = pd.api.types.is_list_like
+
+# start = datetime.datetime(2010, 1, 1)
+# end = datetime.datetime.now()
+
+# SP500 = web.DataReader(['sp500'], 'fred', start, end)
+# SP500.dropna(inplace = True)
+# SP500['daily_return'] = (SP500['sp500']/ SP500['sp500'].shift(1)) -1
+
+# sp500_graph = JenkinsGraph(SP500['daily_return'], "sp500_daily_return")
+# # sp500_graph.local_plot("Daily_return")
+# sp500_graph.export_plot()
 
 # SP500['daily_return'].to_csv(r'sp500_dail_return.csv', index = False)
 #Drop all Not a number values using drop method.
